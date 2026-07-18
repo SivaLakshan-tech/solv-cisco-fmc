cc724aa3eb1cb526da9dbe7a6c99431cee1e8ec4
Sivanandam A <sivaanbueee@gmail.com>
2026-07-18 23:09:01 +0530

    test1

--- tf/fmc_data_host.tf
+++ tf/fmc_data_host.tf
@@ -1,3 +1,6 @@
 data "fmc_host" "example" {
   id = "76d24097-41c4-4558-a4d0-a8c07ac08470"
 }
+data "fmc_host" "example1" {
+  id = "76d24097-41c4-4558-a4d0-a8c07ac08471"
+}
--- tf/fmc_rersource_host.tf
+++ tf/fmc_rersource_host.tf
@@ -4,3 +4,9 @@ resource "fmc_host" "example" {
   ip          = "10.1.1.1"
   overridable = true
 }
+resource "fmc_host" "example1" {
+  name        = "my_host"
+  description = "My Host object"
+  ip          = "10.5.1.1"
+  overridable = true
+}
