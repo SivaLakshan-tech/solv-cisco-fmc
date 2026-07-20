fd5c8acf82c6820aec0e2c193ee2974a02a15cde
SivaLakshan-tech <sivaanbueee@gmail.com>
2026-07-20 22:42:26 +0530

    commit

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    fmc = {
+      source  = "CiscoDevNet/fmc"
+      version = "2.4.1"
+    }
+  }
+}
+
 provider "fmc" {
   username = "admin"
   password = "password"
